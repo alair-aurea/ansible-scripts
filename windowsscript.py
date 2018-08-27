0be25523647e9aec543f2bbebab4bd97a4ced129
Alair Dias Junior <alair.diasjunior@aurea.com>
2018-08-27 17:00:43 -0300

    Refactor to create generic PackagesHandler

--- windowsscript.py
+++ windowsscript.py
@@ -2,54 +2,13 @@ from __future__ import unicode_literals
 from prompt_toolkit import prompt
 from windowsinventorycreator import InventoryCreator
 from windowsplaybookcreator import PlaybookCreator
+from packagehandler import PackageHandler
 import configparser
 import validators
 import inquirer
 import yaml
 
-class WindowsScript():
-
-    def selectPackages( self, host_config ):
-        base_config = configparser.RawConfigParser(allow_no_value=True)
-        base_config.read( "configs/windows.conf" )
-        
-        packages = []
-        default = []
-        
-        config = dict(base_config.items('chocolatey:' + host_config['dev']))
-        
-        for package in config.keys(): 
-            packages.append(package)
-            if (config[package] == 'yes'):
-                default.append(package)
-                print
-            
-        
-        questions = [
-            inquirer.Checkbox('packages',
-                message="Which packages do you want to install?",
-                choices=packages,
-                default=default
-            ),
-        ]
-        answers = inquirer.prompt(questions)
-        
-        return answers[ 'packages' ]
-
-    def additionalPackages( self ):
-      
-        packages = []
-        while(True):
-            additional = prompt('Install any additional package available in chocolatey? (y/n): ', validator=validators.YesNoValidator())
-            if (additional == 'n'):
-                break
-            
-            packages.append(
-                prompt('Package name (this will not be validated by this automation tool): ')
-            )
-            
-        return packages
-          
+class WindowsScript():         
 
     def execute(self, host_config):
         
@@ -59,9 +18,11 @@ class WindowsScript():
         
         print
         
-        host_config[ 'packages' ] = self.selectPackages( host_config )
+        packageHandler = PackageHandler("configs/windows.conf", "chocolatey")
+        
+        host_config[ 'packages' ] = packageHandler.selectPackages( host_config )
                
-        host_config[ 'additional_packages' ] = self.additionalPackages()
+        host_config[ 'additional_packages' ] = packageHandler.selectAdditionalPackages()
         
         inventory_creator = InventoryCreator()
         inventory_creator.create(host_config, "./inventories")
